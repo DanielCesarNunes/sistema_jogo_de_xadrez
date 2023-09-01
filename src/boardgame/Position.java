eae9904a2f4d9cbb201ae4c4b32f4ef845c74699
Daniel Cesar Nunes <nunesdc@hotmail.com>
2023-08-31 22:05:47 -0300

    Implementing possible moves of Rook

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -29,6 +29,11 @@ public class Position {
 	public void setRow(int row) {
 		this.row = row;
 	}
+	
+	public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
 
 	@Override
 	public String toString() {
